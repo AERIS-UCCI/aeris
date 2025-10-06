b2eaeed657f18c6d590221522ae2e3e1f0127dcd
Alexod24 <alex.od2408@gmail.com>
2025-10-05 23:55:01 -0500

    Agregar github

--- aeris-web/src/components/Sections/HeroSection.tsx
+++ aeris-web/src/components/Sections/HeroSection.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
@@ -70,14 +69,13 @@ export default function HeroSection(): JSX.Element {
                   </p>
 
                   <div className="mt-10 flex items-center gap-x-6">
-                    <a
-                      className="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
-                    >
+                    <a className="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600">
                       <Link to="/Rutas">{t("boton.action")}</Link>
                     </a>
                     <a
-                      href="#"
+                      href="https://github.com/AERIS-UCCI/aeris"
                       className="text-sm font-semibold text-gray-900 hover:text-emerald-600"
+                      target="_blank"
                     >
                       {t("boton.informacion")} <span aria-hidden="true">→</span>
                     </a>
